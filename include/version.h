031cb9525e5a28cc89cda4b2c03796a8fdd2ed08
RUAN0007 <ruanpingcheng@gmail.com>
2017-01-10 17:03:30 +0800

    typedef some struct

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 
    @Author: RUAN0007
    @Date:   2017-01-09 18:49:59
-   @Last_Modified_At:   2017-01-09 19:20:09
+   @Last_Modified_At:   2017-01-10 09:36:01
    @Last_Modified_By:   RUAN0007
 
 */
@@ -16,17 +16,26 @@
 #include <map>
 #include <vector>
 #include <boost/dynamic_bitset.hpp>
+
 namespace ustore{
+namespace relation{
 
 typedef version_t CommitID;
 
 //RecordID uniquely identifies a tuple's location in Ustore.
-struct RecordID{
+typedef struct {
 	version_t version; //the version_t of the page that contains this tuple
 	unsigned tuple_index; // the tuple's index in this page, starting with 0. 
-};
+} RecordID;
 
-struct Commit{
+typedef struct {
+
+	// Commit(CommitID init_id, version_t init_start, version_t init_end){
+	// 	id = init_id;
+	// 	start_version = init_start;
+	// 	end_version = init_end;
+	// }
+	
 	CommitID id; //id of this commit. Shall be the version_t of start_version
 	version_t start_version; //first version_t of the commited page
 	version_t end_version; //last version_t of the commited page
@@ -34,9 +43,9 @@ struct Commit{
 	//mapping pairs of tuple bit position to tuple's location in Ustore
 	std::map<unsigned, RecordID> tuple_positions;
     boost::dynamic_bitset<> tuple_presence; //Bitmap to indicate the tuple's presence in this commit
-};
+} Commit;
 
-struct Branch{
+typedef struct Branch{
 	std::string branch_name;// this branch name
 	std::string base_branch; // the base branch from which this branch originates
 
@@ -48,10 +57,9 @@ struct Branch{
 	void new_commit(CommitID commit_id){
 		commit_ids.push_back(commit_id);
 	}
-};
+} Branch;
 
-
-
+}
 }
 
 
