c241cdea17e4484b63fa8b46080c96b555cad952
RUAN0007 <ruanpingcheng@gmail.com>
2017-01-10 17:04:31 +0800

    allow to set buffer for page

--- include/page.h
+++ include/page.h
@@ -2,7 +2,7 @@
 
    @Author: RUAN0007
    @Date:   2017-01-07 09:48:57
-   @Last_Modified_At:   2017-01-10 15:09:42
+   @Last_Modified_At:   2017-01-10 16:27:03
    @Last_Modified_By:   RUAN0007
 
 */
@@ -38,6 +38,8 @@ public:
 
 	inline unsigned GetTupleNumber() const{ return tuple_num_;}
 
+	void SetData(unsigned char* buffer, size_t page_size);
+
 	void Reset();
 
 	//return 0 if index > tuple_num_
@@ -47,7 +49,7 @@ public:
 	//Return the tuple position in this page
 
 	//Return -1 if insertion fails. 
-	int InsertTuple(Tuple* tuple, std::string& msg);
+	int InsertTuple(const Tuple* tuple, std::string& msg);
 
 private:
 	std::string table_name_;
--- src/page.cpp
+++ src/page.cpp
@@ -2,7 +2,7 @@
 
    @Author: RUAN0007
    @Date:   2017-01-07 13:21:13
-   @Last_Modified_At:   2017-01-10 15:13:45
+   @Last_Modified_At:   2017-01-10 16:26:45
    @Last_Modified_By:   RUAN0007
 
 */
@@ -39,8 +39,15 @@ Page::Page(string table_name, const TupleDscp* tuple_schema, unsigned char* buff
 
 	this->tuple_num_ = *reinterpret_cast<int*>(buffer);
 	this->empty_slot_offset_ = sizeof(int) + this->tuple_num_ * this->tuple_schema_->GetTupleSize();
-
 }
+
+void Page::SetData(unsigned char* buffer, size_t page_size){
+	this->buffer_ = buffer;
+	this->page_size_ = page_size;
+	this->tuple_num_ = *reinterpret_cast<int*>(buffer);
+	this->empty_slot_offset_ = sizeof(int) + this->tuple_num_ * this->tuple_schema_->GetTupleSize();
+
+} 
 
 Page::~Page() { delete[] this->buffer_;}
 
@@ -63,7 +70,7 @@ Tuple* Page::GetTuple(unsigned index) const {
 }
 
 
-int Page::InsertTuple(Tuple* tuple, string& msg) {
+int Page::InsertTuple(const Tuple* tuple, string& msg) {
 
 	size_t tuple_size = this->tuple_schema_->GetTupleSize();
 
