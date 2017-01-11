d4cc93526d8353815dbaceb1407da2f543c995ec
RUAN0007 <ruanpingcheng@gmail.com>
2017-01-11 16:36:40 +0800

    pass test for branch

--- test/test_relation.cpp
+++ test/test_relation.cpp
@@ -2,7 +2,7 @@
 
    @Author: RUAN0007
    @Date:   2017-01-11 09:22:13
-   @Last_Modified_At:   2017-01-11 14:47:20
+   @Last_Modified_At:   2017-01-11 16:35:30
    @Last_Modified_By:   RUAN0007
 
 */
@@ -42,9 +42,9 @@ class RelationTest : public ::testing::Test {
   	schema = new TupleDscp(relation_name, types, names);
 
 
-	read_page = new Page(relation_name, schema, 1024);
+	read_page = new Page(relation_name, schema, 4096);
 
-	write_page = new Page(relation_name, schema, 1024);
+	write_page = new Page(relation_name, schema, 4096);
 	storage = new UstoreHeapStorage(relation_name, *schema, &client, write_page); 
 
 	tuple_store = new unsigned char[4000];
@@ -360,6 +360,130 @@ TEST_F(RelationTest, OperationAfterRemove) {
 
 	delete int_f1; 
 	delete int_f2;
+	delete str_f1;
+	delete str_f2;
+}
+
+TEST_F(RelationTest, Branch) {
+
+	string msg;
+	CommitID commit_id;
+
+	Field* int_f1 = new IntField(1);
+	Field* int_f2 = new IntField(2);
+	Field* int_f3 = new IntField(3);
+
+	Field* str_f1 = new StrField("1");
+	Field* str_f2 = new StrField("2");
+	Field* str_f3 = new StrField("3");
+
+	Tuple t1(tuple_store, 0, schema);	
+	t1.SetFieldByIndex(0, int_f1, &msg);
+	t1.SetFieldByIndex(1, str_f1, &msg);
+
+	Tuple t21(tuple_store, 500, schema);	
+	t21.SetFieldByIndex(0, int_f2, &msg);
+	t21.SetFieldByIndex(1, str_f1, &msg);
+
+	Tuple t22(tuple_store, 1000, schema);	
+	t22.SetFieldByIndex(0, int_f2, &msg);
+	t22.SetFieldByIndex(1, str_f2, &msg);
+
+	Tuple t3(tuple_store, 1500, schema);	
+	t3.SetFieldByIndex(0, int_f3, &msg);
+	t3.SetFieldByIndex(1, str_f3, &msg);
+
+	ASSERT_TRUE(storage->InsertTuple(&t1, &msg)) << msg;
+	ASSERT_TRUE(storage->Commit(&commit_id, &msg)) << msg;
+
+	string master_branch = "master";
+	string branch1 = "branch1";
+
+	EXPECT_TRUE(storage->Branch(master_branch, branch1, &msg));
+	EXPECT_STREQ(storage->GetCurrentBranchInfo().branch_name.c_str(), branch1.c_str());
+	EXPECT_STREQ(storage->GetCurrentBranchInfo().base_branch.c_str(), master_branch.c_str());
+
+	ASSERT_TRUE(storage->InsertTuple(&t21, &msg)) << msg;
+	ASSERT_TRUE(storage->Commit(&commit_id, &msg)) << msg;
+
+	ASSERT_TRUE(storage->InsertTuple(&t3, &msg)) << msg;
+	ASSERT_TRUE(storage->Commit(&commit_id, &msg)) << msg;
+
+	EXPECT_FALSE(storage->Switch("Non Existent Branch", &msg)) << msg;
+
+	ASSERT_TRUE(storage->Switch(master_branch, &msg));
+	EXPECT_STREQ(storage->GetCurrentBranchInfo().branch_name.c_str(), master_branch.c_str());
+	EXPECT_STREQ(storage->GetCurrentBranchInfo().base_branch.c_str(), master_branch.c_str());
+
+	ASSERT_TRUE(storage->InsertTuple(&t22, &msg)) << msg;
+	ASSERT_TRUE(storage->Commit(&commit_id, &msg)) << msg;
+
+/*
+
+t1 -> t21 -> t3 (branch1)
+ | -> t22 (master)
+
+*/
+	Tuple *pt1,*pt2,*pt3;
+
+//Common tuple pt1 from both branches
+	pt1 = storage->GetTuple(master_branch, int_f1, read_page, &msg);
+	EXPECT_NE(pt1, reinterpret_cast<Tuple*>(0)) << msg;
+	EXPECT_EQ(t1, *pt1);
+	delete pt1;
+
+	pt1 = storage->GetTuple(branch1, int_f1, read_page, &msg);
+	EXPECT_NE(pt1, reinterpret_cast<Tuple*>(0)) << msg;
+	EXPECT_EQ(t1, *pt1);
+	delete pt1;
+
+//diffent tuple pt2 with same pk from both branches
+	pt2 = storage->GetTuple(master_branch, int_f2, read_page, &msg);
+	EXPECT_NE(pt1, reinterpret_cast<Tuple*>(0)) << msg;
+	EXPECT_EQ(t22, *pt2);
+	delete pt2;
+
+	pt2 = storage->GetTuple(branch1, int_f2, read_page, &msg);
+	EXPECT_NE(pt1, reinterpret_cast<Tuple*>(0)) << msg;
+	EXPECT_EQ(t21, *pt2);
+	delete pt2;
+
+	pt3 = storage->GetTuple(master_branch, int_f3, read_page, &msg);
+	EXPECT_EQ(pt3, reinterpret_cast<Tuple*>(0)) << msg;
+
+	pt3 = storage->GetTuple(branch1, int_f3, read_page, &msg);
+	EXPECT_NE(pt3, reinterpret_cast<Tuple*>(0)) << msg;
+	EXPECT_EQ(t3, *pt3);
+	delete pt3;
+
+
+/*
+Merge: 
+t1 -> t21 -> t3 (branch1)
+ | -> t22 --- | -> master
+
+*/
+	ASSERT_TRUE(storage->Merge(&commit_id, master_branch, branch1, &msg)) << msg;
+
+	pt1 = storage->GetTuple(master_branch, int_f1, read_page, &msg);
+	EXPECT_NE(pt1, reinterpret_cast<Tuple*>(0)) << msg;
+	EXPECT_EQ(t1, *pt1);
+	delete pt1;
+
+	pt2 = storage->GetTuple(master_branch, int_f2, read_page, &msg);
+	EXPECT_NE(pt1, reinterpret_cast<Tuple*>(0)) << msg;
+	EXPECT_EQ(t22, *pt2);
+	delete pt2;
+
+	pt3 = storage->GetTuple(master_branch, int_f3, read_page, &msg);
+	EXPECT_NE(pt3, reinterpret_cast<Tuple*>(0)) << msg;
+	EXPECT_EQ(t3, *pt3);
+	delete pt3;
+
+	delete int_f1;
+	delete int_f2;
+	delete int_f3;
+
 	delete str_f1;
 	delete str_f2;
 }
